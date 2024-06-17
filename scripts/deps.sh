35cab62030f882281b66c5ff057fd471e5a7c94a
FabrizioCafolla <developer@fabriziocafolla.com>
2024-06-17 22:38:31 +0200

    feat(infrastructure): add doppler secrets

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -12,11 +12,7 @@ set -o pipefail
 
 WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
 
-main(){
-  echo "## Setup: global project deps"
-
-  cd "${WORKDIR}"
-
+setup_virtualenv(){
   mkdir -pv .venv
 
   if [[ "$(python3 --version)" == *"Python 3.12"* ]] ; then
@@ -25,11 +21,30 @@ main(){
     pip3 install --upgrade virtualenv
     python3 -m virtualenv .venv
   fi
+}
 
-  # shellcheck disable=SC1091
-  source .activate
+main(){
+  echo "[INFO] Installing base dependencies"
 
+  local _arg_virtualenv=${1:-true}
+  local _arch
+  _arch=$(uname -m)
+
+  cd "${WORKDIR}"
+
+  if [[ "${_arg_virtualenv}" == "true" ]]; then
+    setup_virtualenv
+
+    # shellcheck disable=SC1091
+    . .activate
+  fi
+
+  python3 -m pip install --upgrade pip
+  python3 -m pip install wheel
+
+  echo "Installing requirements"
   pip3 install -r requirements.txt
+
   pre-commit install
 }
 
